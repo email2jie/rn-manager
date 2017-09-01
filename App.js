755631aadd88bbea32b860c4d0f77c5a30289e21
Jie Wei <email2jie@gmail.com>
2017-08-31 23:02:25 -0700

    configured firebase auth

--- App.js
+++ App.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './src/reducers';
 
+
 class App extends Component {
+  componentWillMount(){
+    const config = {
+    apiKey: "AIzaSyCplaJg9FaxhW8nXcGZkGACvaELMAQXJ6Q",
+    authDomain: "rn-manager-ff996.firebaseapp.com",
+    databaseURL: "https://rn-manager-ff996.firebaseio.com",
+    projectId: "rn-manager-ff996",
+    storageBucket: "",
+    messagingSenderId: "408271946511"
+    }
+
+
+    firebase.initializeApp(config);
+  
+  }
 
   render() {
     return (
